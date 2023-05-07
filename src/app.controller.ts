b20187302f89057ca6e0a3cd53f2658d006ca2d4
dayasudhan <dayasudhankg@gmail.com>
2023-05-07 15:32:06 +0530

    client not working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Get, Post, UseGuards,Res } from '@nestjs/common';
 import { AppService } from './app.service';
 //import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import * as path from 'path';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService,private authService: AuthService) {}
 
   @Get()
-  getHello(): string {
-    console.log("process.env.ATLAS_URISTRING",process.env.ATLAS_URISTRING)
-    return this.appService.getHello();
+  serveReactApp(@Res() res: any) {
+    console.log("serveReactApp")
+    res.sendFile(path.join(__dirname, '..', "client\\public", 'index.html'));
   }
+
+  // @Get()
+  // getHello(): string {
+  //   console.log("process.env.ATLAS_URISTRING",process.env.ATLAS_URISTRING)
+  //   return this.appService.getHello();
+  // }
 
   //@UseGuards(AuthGuard('local'))
   @UseGuards(LocalAuthGuard)
